c58af0a3b93a80de954d6cc14f4b47913d53bfad
Lev Dovzhik <lev.dovzhik@corp.vk.com>
2020-11-15 20:06:16 +0300

    Add tests for assigment

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ struct throwing_default_t {
 
 struct throwing_move_t {
   throwing_move_t(throwing_move_t &&) noexcept(false) { throw std::exception(); }
+  throwing_move_t &operator=(throwing_move_t &&) = default;
 };
 
 struct no_copy_t {
@@ -32,6 +33,28 @@ struct non_trivial_copy_t {
   non_trivial_copy_t(const non_trivial_copy_t &other) noexcept : x{other.x + 1} {}
 
   int x;
+};
+
+struct non_trivial_copy_assigment_t {
+  non_trivial_copy_assigment_t &operator=(const non_trivial_copy_assigment_t &other) {
+    x = other.x + 1;
+    return *this;
+  };
+
+  int x;
+};
+
+struct no_move_assigment_t {
+  no_move_assigment_t &operator=(no_move_assigment_t &&) = delete;
+};
+
+struct no_copy_assigment_t {
+  no_copy_assigment_t &operator=(const no_copy_assigment_t &) = delete;
+};
+
+struct throwing_move_assigment_t {
+  throwing_move_assigment_t(throwing_move_assigment_t &&) = default;
+  throwing_move_assigment_t &operator=(throwing_move_assigment_t &&) noexcept(false) { throw std::exception(); }
 };
 
 TEST(traits, destructor) {
@@ -124,4 +147,61 @@ TEST(traits, in_place_index) {
   ASSERT_TRUE(construct4);
   ASSERT_TRUE(construct5);
   ASSERT_TRUE(construct6);
+}
+
+TEST(traits, copy_assigment) {
+  using variant1 = variant<std::string, double, no_copy_t>;
+  using variant2 = variant<std::vector<short>, int, no_copy_assigment_t>;
+  using variant3 = variant<dummy_t, int, non_trivial_copy_assigment_t>;
+  using variant4 = variant<double, non_trivial_copy_t, bool>;
+  using variant5 = variant<int, short, char, dummy_t, bool>;
+  ASSERT_FALSE(std::is_copy_assignable_v<variant1>);
+  ASSERT_FALSE(std::is_copy_assignable_v<variant2>);
+  ASSERT_TRUE(std::is_copy_assignable_v<variant3>);
+  ASSERT_TRUE(std::is_copy_assignable_v<variant4>);
+  ASSERT_TRUE(std::is_copy_assignable_v<variant5>);
+  ASSERT_FALSE(std::is_trivially_copy_assignable_v<variant3>);
+  ASSERT_FALSE(std::is_trivially_copy_assignable_v<variant4>);
+  ASSERT_TRUE(std::is_trivially_copy_assignable_v<variant5>);
+}
+
+TEST(traits, move_assigment) {
+  using variant1 = variant<std::string, double, no_move_t>;
+  using variant2 = variant<int, std::vector<std::string>, no_move_assigment_t, bool>;
+  using variant3 = variant<dummy_t, int, std::vector<double>>;
+  using variant4 = variant<double, std::string, bool>;
+  using variant5 = variant<int, short, char, dummy_t, bool>;
+  using variant6 = variant<int, std::string, throwing_move_t, double>;
+  using variant7 = variant<int, throwing_move_assigment_t, double>;
+  ASSERT_FALSE(std::is_move_assignable_v<variant1>);
+  ASSERT_FALSE(std::is_move_assignable_v<variant2>);
+  ASSERT_TRUE(std::is_move_assignable_v<variant3>);
+  ASSERT_TRUE(std::is_move_assignable_v<variant4>);
+  ASSERT_TRUE(std::is_move_assignable_v<variant5>);
+  ASSERT_FALSE(std::is_trivially_move_assignable_v<variant3>);
+  ASSERT_FALSE(std::is_trivially_move_assignable_v<variant4>);
+  ASSERT_TRUE(std::is_trivially_move_assignable_v<variant5>);
+  ASSERT_TRUE(std::is_move_assignable_v<variant6>);
+  ASSERT_TRUE(std::is_move_assignable_v<variant7>);
+  ASSERT_FALSE(std::is_nothrow_move_assignable_v<variant6>);
+  ASSERT_FALSE(std::is_nothrow_move_assignable_v<variant7>);
+  ASSERT_TRUE(std::is_nothrow_move_assignable_v<variant3>);
+  ASSERT_TRUE(std::is_nothrow_move_assignable_v<variant4>);
+  ASSERT_TRUE(std::is_nothrow_move_assignable_v<variant5>);
+}
+
+TEST(traits, converting_assigment) {
+  using variant1 = variant<std::string, std::vector<char>, bool>;
+  bool assigment1 = std::is_assignable_v<variant1 &, std::string &&>;
+  bool assigment2 = std::is_assignable_v<variant1 &, const char *>;
+  bool assigment3 = std::is_assignable_v<variant1 &, size_t>;
+  bool assigment4 = std::is_nothrow_assignable_v<variant1 &, std::string &&>;
+  bool assigment5 = std::is_nothrow_assignable_v<variant1 &, const std::string &>;
+  bool assigment6 = std::is_assignable_v<variant1 &, double *>;
+  ASSERT_TRUE(assigment1);
+  ASSERT_TRUE(assigment2);
+  ASSERT_FALSE(assigment3);
+  ASSERT_TRUE(assigment4);
+  ASSERT_FALSE(assigment5);
+  ASSERT_FALSE(assigment6);
 }
